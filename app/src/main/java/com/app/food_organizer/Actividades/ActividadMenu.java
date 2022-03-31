ec78b113fe76dad0b89f627acadbc520c076c7df
PatricioGonz <64499562+PatricioGonz@users.noreply.github.com>
2022-03-30 22:54:53 -0600

    activities created

---
+++ app/src/main/java/com/app/food_organizer/Actividades/ActividadMenu.java
@@ -0,0 +1,14 @@
+package com.app.food_organizer.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_menu);
+    }
+}
