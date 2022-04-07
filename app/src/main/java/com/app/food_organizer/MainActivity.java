930c4323f9f0cd7f66ca7eea3462ff94ca43759a
David Fernandez Belden <DavidFDot@gmail.com>
2022-04-06 20:17:21 -0500

    Se Borro Todo

--- app/src/main/java/com/app/food_organizer/MainActivity.java
+++ app/src/main/java/com/app/food_organizer/MainActivity.java
@@ -1,10 +1,13 @@
 package com.app.food_organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView mCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
